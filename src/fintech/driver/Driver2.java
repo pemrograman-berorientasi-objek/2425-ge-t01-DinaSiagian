f61b59b38b90580d4268b17950471eaae46a3c3d
DinaSiagian <143679301+DinaSiagian@users.noreply.github.com>
2025-02-07 17:40:12 +0700

    pair 24

--- src/fintech/driver/Driver2.java
+++ src/fintech/driver/Driver2.java
@@ -1,15 +1,36 @@
 package fintech.driver;
+import java.util.Scanner;
+import fintech.model.Transaction;
+import fintech.model.Account;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S23009_Dina Marlina Siagian
+ * @author 12S23028_Daniel Situmorang
  */
 public class Driver2 {
 
     public static void main(String[] _args) {
 
-        // codes
+        Scanner input = new Scanner(System.in);
+        String command = input.nextLine();
+        String owner = input.nextLine();
+        String account_nama = input.nextLine();
+        Account account = new Account(command, owner, account_nama);
+        System.out.println(account.toString());
 
+        String command2 = input.nextLine();
+        account_nama = input.nextLine();
+        Double amount = input.nextDouble();
+        input.nextLine();
+        String posted_at = input.nextLine();
+        String note = input.nextLine();
+        Double balance = account.getBalance();
+        int id = 1;
+        Transaction transaction = new Transaction( id, account.getAccountNama(), posted_at, amount, note, balance);
+        input.close();
+
+    
+        System.out.println(transaction.toString());
     }
 
 }
--- src/fintech/model/Transaction.java
+++ src/fintech/model/Transaction.java
@@ -1,11 +1,49 @@
 package fintech.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S23009_Dina Marlina Siagian
+ * @author 12S23028_Daniel Situmorang
  */
 public class Transaction {
 
-    // class definition
+    private int id;
+    private String account_name;
+    private String posted_at;
+    private double amount;
+    private String note;
+    private double balance;
 
+    public Transaction(int _id, String _account_name, String _posted_at, double _amount, String _note, double _balance) {
+        this.id = _id;
+        this.account_name = _account_name;
+        this.posted_at = _posted_at;
+        this.amount = _amount;
+        this.note = _note;
+        this.balance = _balance + _amount;
+    }
+
+    public String getAccountName() {
+        return this.account_name;
+    }
+
+    public String getPostedAt() {
+        return this.posted_at;
+    }
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public String getNote() {
+        return this.note;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + "|" + this.account_name + "|" + this.amount + "|" + this.posted_at + "|" + this.note + "|" + this.balance;
+    }
 }
